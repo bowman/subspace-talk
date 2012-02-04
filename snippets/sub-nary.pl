c05ff43e54dbef9c0a48c4e9b0b51ca73a8b8425
Brad Bowman <bsb@bereft.net>
2012-02-05 00:29:08 +1100

    External files and sys to run

--- snippets/sub-nary.pl
+++ snippets/sub-nary.pl
@@ -1,10 +1,9 @@
-#!/usr/bin/perl 
-  use Sub::Nary; use YAML::XS;
+  use Sub::Nary; use JSON::XS;
 
   sub x {
     if ($ENV{one})     { return 1;     }
     elsif (rand()<0.5) { return (1,2); }
     else               { return @ARGV; }
   }
-
-  print Dump(Sub::Nary->new->nary(\&x));
+  print encode_json(
+            Sub::Nary->new->nary(\&x) );
