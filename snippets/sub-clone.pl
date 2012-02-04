3268ecb65d8d148ff1871966caa0fbd7df31f02f
Brad Bowman <bsb@bereft.net>
2012-02-05 00:40:01 +1100

    sub-clone fits now

--- snippets/sub-clone.pl
+++ snippets/sub-clone.pl
@@ -1,9 +1,6 @@
   use Sub::Clone;
   sub get_callback { return sub { "hi!" }; }
-
-  my $first = get_callback();
-  my $second = get_callback();
+  my $first = get_callback(); my $second = get_callback();
   my $third = clone_sub $second;
-
-  warn "$first == $second"; # prints the same refaddr
-  warn "$third != $second"; # prints the diff refaddr
+  print "# $first == $second\n"; # same refaddr
+  print "# $third != $second\n"; # diff refaddr
